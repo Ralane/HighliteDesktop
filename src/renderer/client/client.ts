d368a9533c7d68b7a1273eebaa2c918bd4f4da13
Riley Lane <rileylanea@gmail.com>
2025-07-15 17:56:16 -0700

    feat: New QuickActionMouseTooltip plugin

--- src/renderer/client/client.ts
+++ src/renderer/client/client.ts
@@ -28,6 +28,7 @@ import { EntityHighlight } from './highlite/plugins/EntityHighlight';
 import { InventoryTooltips } from './highlite/plugins/InventoryTooltips';
 import { PacketQueue } from './highlite/plugins/PacketQueue';
 import { ChatEnhancer } from './highlite/plugins/ChatEnhancer';
+import { QuickActionMouseTooltip } from './highlite/plugins/QuickActionMouseTooltip';
 
 import '@iconify/iconify';
 import '@static/css/index.css';
@@ -67,6 +68,7 @@ const PLUGIN_REGISTRY = [
     { class: AutoSprint, path: './highlite/plugins/AutoSprint' },
     { class: InventoryTooltips, path: './highlite/plugins/InventoryTooltips' },
     { class: ChatEnhancer, path: './highlite/plugins/ChatEnhancer' },
+    { class: QuickActionMouseTooltip, path: './highlite/plugins/QuickActionMouseTooltip' },
 ];
 
 async function obtainGameClient() {
